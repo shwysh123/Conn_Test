c638d1f6ff242b13a5c381e74c62962463b4bc58
BBAK <shwysh123@naver.com>
2023-08-25 13:26:38 +0900

    TEST

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace _01BASIC
+{
+    partial class winform
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private string selectedPort = "";
+        private bool isConnected = false;
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortNumber = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.conn_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PortNumber
+            // 
+            this.PortNumber.BackColor = System.Drawing.Color.White;
+            this.PortNumber.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PortNumber.FormattingEnabled = true;
+            this.PortNumber.Items.AddRange(new object[] {
+            "COM1",
+            "COM2"});
+            this.PortNumber.Location = new System.Drawing.Point(29, 40);
+            this.PortNumber.Name = "PortNumber";
+            this.PortNumber.Size = new System.Drawing.Size(75, 20);
+            this.PortNumber.TabIndex = 0;
+            this.PortNumber.SelectedIndexChanged += new System.EventHandler(this.PortNumber_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LED_01 ON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "LED_01 OFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "LED_02 ON";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(123, 131);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "LED_02 OFF";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // conn_btn
+            // 
+            this.conn_btn.Location = new System.Drawing.Point(123, 38);
+            this.conn_btn.Name = "conn_btn";
+            this.conn_btn.Size = new System.Drawing.Size(75, 23);
+            this.conn_btn.TabIndex = 5;
+            this.conn_btn.Text = "연결";
+            this.conn_btn.UseVisualStyleBackColor = true;
+            this.conn_btn.Click += new System.EventHandler(this.conn_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Message : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 7;
+            // 
+            // winform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(230, 227);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.conn_btn);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PortNumber);
+            this.Name = "winform";
+            this.Text = "winform";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox PortNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button conn_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace _01BASIC
+{
+    public partial class winform : Form
+    {   
+        private SerialPort serialPort = new SerialPort();
+        public winform()
+        {
+            InitializeComponent();
+        }
+        private void PortNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void SerialPort_DataRecv(object sender, SerialDataReceivedEventArgs e)
+        {
+            String recvData = this.serialPort.ReadExisting();
+            Console.WriteLine(recvData);
+        }
+
+       
+
+
+        private void conn_btn_Click(object sender, EventArgs e)
+        {
+            string selectedPortName = this.PortNumber.Items[this.PortNumber.SelectedIndex].ToString();
+            Console.WriteLine(selectedPortName + " CONN");
+            label2.Text = selectedPortName + " CONN";
+            try
+            {
+                this.serialPort.PortName = selectedPortName;
+                this.serialPort.BaudRate = 9600;
+                this.serialPort.DataBits = 8;
+                this.serialPort.StopBits = System.IO.Ports.StopBits.One;
+                this.serialPort.Parity = System.IO.Ports.Parity.None;
+                this.serialPort.Open();
+                this.serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataRecv);
+            }
+            catch
+            {
+                this.serialPort.Close();
+            }
+
+        } 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Console.WriteLine(button1.Text + " CLICKED");
+            label2.Text = button1.Text+" SUCCESS";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(button2.Text + " CLICKED");
+            label2.Text = button2.Text + " SUCCESS";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(button3.Text + " CLICKED");
+            label2.Text = button3.Text + " SUCCESS";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(button4.Text + " CLICKED");
+            label2.Text = button4.Text + " SUCCESS";
+        }
+    }
+}
